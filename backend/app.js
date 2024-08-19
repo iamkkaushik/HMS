51ca0053b6d0c760d9a74f381078e6cc09c6cf1b
iamkkaushik <160194586+iamkkaushik@users.noreply.github.com>
2024-08-19 12:37:54 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -11,16 +11,16 @@ import appointmentRouter from "./router/appointmentRouter.js";
 config();
 const app = express();
 
-// app.use(
-//   cors({
-//     // origin: [process.env.FRONTEND_URL1,process.env.FRONTEND_URL2],
-//     origin: [],
-//     method: ["GET", "POST", "DELETE", "PUT"],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL1,process.env.FRONTEND_URL2],
+    // origin: [],
+    method: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
 
-app.use(cors());
+// app.use(cors());
 
 //config({ path: "./config.env" });
 app.use(cookieParser());
