817069064df13f7a4d5a062024c453dec4085d04
iamkkaushik <160194586+iamkkaushik@users.noreply.github.com>
2024-08-19 11:37:40 +0530

    Update Login.jsx

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -18,7 +18,8 @@ const Login = () => {
     try {
       await axios
         .post(
-          "http://localhost:3000/api/v1/user/login",
+          "hms-delta-sepia.vercel.app
+/api/v1/user/login",
           { email, password, confirmPassword, role: "Patient" },
           {
             withCredentials: true,
