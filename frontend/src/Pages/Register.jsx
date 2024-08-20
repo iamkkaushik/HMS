29117e7231116f957a2dc8b13cb3fabdd0ea7749
iamkkaushik <160194586+iamkkaushik@users.noreply.github.com>
2024-08-20 13:05:15 +0530

    Update Register.jsx

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -23,7 +23,7 @@ const Register = () => {
     try {
       await axios
         .post(
-          "https://hms-delta-sepia.vercel.app/api/v1/user/patient/register",
+          "https://hms-1-bu8j.onrender.com/api/v1/user/patient/register",
           { firstName, lastName, email, phone, nic, dob, gender, password },
           {
             withCredentials: true,
