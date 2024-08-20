28d86904a7711b00711489e4b90a168bf214afc9
iamkkaushik <160194586+iamkkaushik@users.noreply.github.com>
2024-08-20 15:20:30 +0530

    Update dbConnection.js

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -22,17 +22,29 @@
 
 
 // Connect to the database first
+// import mongoose from 'mongoose';
+
+// export const dbConnection = async () => {
+//     try {
+//         await mongoose.connect(process.env.MONGODB_URI, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         });
+//         console.log('Connected to MongoDB');
+//     } catch (err) {
+//         console.error('Failed to connect to MongoDB:', err);
+//     }
+// };
+
 import mongoose from 'mongoose';
 
 export const dbConnection = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log('Connected to MongoDB');
     } catch (err) {
         console.error('Failed to connect to MongoDB:', err);
+        process.exit(1); // Exit the process if the database connection fails
     }
 };
 
